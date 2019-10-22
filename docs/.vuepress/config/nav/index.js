42fa64911fc8f524af9cf891881cc8785405e80f
Tinnnny <847022952@qq.com>
2019-10-22 08:57:24 +0800

    init

---
+++ docs/.vuepress/config/nav/index.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {text: 'Home', link: '/', icon: 'reco-home'},
+  {text: 'TimeLine', link: '/timeline/', icon: 'reco-date'},
+  {
+    text: 'Contact',
+    icon: 'reco-message',
+    items: [
+      {text: 'GitHub', link: 'https://github.com/recoluan', icon: 'reco-github'},
+      {text: '简书', link: 'https://www.jianshu.com/u/cd674a19515e', icon: 'reco-jianshu'},
+      {text: 'WeChat', link: 'https://mp.weixin.qq.com/s/mXFqeUTegdvPliXknAAG_A', icon: 'reco-wechat'},
+    ]
+  }
+];
